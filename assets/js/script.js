9481e7f402c07d99342a3d5565c7997790483497
Elisa <elisa.salvatore110@gmail.com>
2024-07-06 16:56:06 +0200

    update: new code implemented for changing theme (dark mode) and  add: local storage for selected theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// back to the top button
+//-- back to the top button
 const backTopBtn = document.querySelector( '.btn-back-top' );
 window.addEventListener( 'scroll', checkHeight );
 
@@ -17,28 +17,36 @@ backTopBtn.addEventListener( 'click', () => {
     } );
 } );
 
-// dark mode
-var checkbox = document.querySelector( 'input[name=mode]' );
+//-- dark mode and local storage
+const checkbox = document.getElementById( "changeTheme" ); //get the checkbox to a variable
 
-checkbox.addEventListener( 'change', function () {
-    if ( this.checked ) {
-        trans();
-        document.documentElement.setAttribute( 'data-theme', 'dark' );
-        document.querySelector( ".navbar" ).setAttribute( 'data-theme', 'dark' );
-        document.querySelector( "footer" ).setAttribute( 'data-theme', 'dark' );
-        document.querySelector( ".offcanvas" ).setAttribute( 'data-theme', 'dark' );
+//check storage if dark mode was on or off
+if ( sessionStorage.getItem( "mode" ) == "dark" ) {
+    darkmode(); //if dark mode was on, run this function
+} else {
+    nodark(); //else run this function
+}
+
+//if the checkbox state is changed, run a function
+checkbox.addEventListener( "change", function () {
+    //check if the checkbox is checked or not
+    if ( checkbox.checked ) {
+        darkmode(); //if the checkbox is checked, run this function
     } else {
-        trans();
-        document.documentElement.setAttribute( 'data-theme', 'light' );
-        document.querySelector( ".navbar" ).setAttribute( 'data-theme', 'light' );
-        document.querySelector( "footer" ).setAttribute( 'data-theme', 'light' );
-        document.querySelector( ".offcanvas" ).setAttribute( 'data-theme', 'light' );
+        nodark(); //else run this function
     }
 } );
 
-let trans = () => {
-    document.documentElement.classList.add( 'transition' );
-    window.setTimeout( () => {
-        document.documentElement.classList.remove( 'transition' );
-    }, 1000 );
-};
+//function for checkbox when checkbox is checked
+function darkmode() {
+    document.body.classList.add( "dark-mode" ); //add a class to the body tag
+    checkbox.checked = true; //set checkbox to be checked state
+    sessionStorage.setItem( "mode", "dark" ); //store a name & value to know that dark mode is on
+}
+
+//function for checkbox when checkbox is not checked
+function nodark() {
+    document.body.classList.remove( "dark-mode" ); //remove added class from body tag
+    checkbox.checked = false; //set checkbox to be unchecked state
+    sessionStorage.setItem( "mode", "light" ); //store a name & value to know that dark mode is off or light mode is on
+}
